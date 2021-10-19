3e26c1cadeeb47bcb36c487234a1a5862fc4c07c
ccppoo <shmishmi79@gmail.com>
2021-10-19 23:53:09 +0900

    add GUI case

--- chat/argparser.py
+++ chat/argparser.py
@@ -2,12 +2,19 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument("name", help="user name", type=str)
 parser.add_argument("--dev", help="enables dev mode", action="store_true")
+
 
 args = parser.parse_args()
 
 if args.dev:
-    print("dev mode")
     DEV = True
 else:
     DEV = False
+
+NAME = args.name
+
+if __name__ == '__main__':
+    print(f"dev mode : {DEV}")
+    print(f'name : {NAME}')
