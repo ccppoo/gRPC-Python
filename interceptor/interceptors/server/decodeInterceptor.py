9cb20119f58b0ae758b883aaced7883e6e8eab1b
ccppoo <shmishmi79@gmail.com>
2021-11-30 14:09:17 +0900

    remove script not used

--- interceptor/interceptors/server/decodeInterceptor.py
+++
@@ -1,9 +0,0 @@
-from grpc import ServerInterceptor
-
-
-class DecodeInterceptor(ServerInterceptor):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def intercept_service(self, continuation, handler_call_details):
-        return continuation(handler_call_details)
