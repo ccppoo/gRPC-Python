5ac81398f7aea9addc2966c2fdcff757681aaac8
ccppoo <shmishmi79@gmail.com>
2021-11-30 15:15:09 +0900

    add 'Detail' namedtuple
    Simple handmade namedtuple for accessing stub method

--- interceptor/proto_modules/__init__.py
+++ interceptor/proto_modules/__init__.py
@@ -1,2 +1,15 @@
 import sys
+from collections import namedtuple
 sys.path.append('proto_modules')
+
+method = namedtuple('_method', field_names=['name', 'type'])
+details = namedtuple('_details', field_names=[
+                     'pingRequest', 'sendMessage', 'addTheseNumbers'])
+
+# accessible method name generated script from `*_pb2_grpc.py`
+# By doing this, method could be dispatched for specific interceptors
+SecureMessageDetail = details(
+    method(r'/interceptors.SecureMessage/PingRequest', 'unary_unary'),
+    method(r'/interceptors.SecureMessage/sendMessage', 'unary_unary'),
+    method(r'/interceptors.SecureMessage/addTheseNumbers', 'stream_unary')
+)
